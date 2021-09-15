5a041f7caa220a2ff2456a439d250732ac1e917e
Kollier Martin <kollierm@yahoo.com>
2021-09-15 16:01:49 -0400

    Code for To Do List

---
+++ Projects/Project 0/src/main/java/Utils/PrintView.java
@@ -0,0 +1,19 @@
+package Utils;
+
+import Models.ToDoItem;
+
+public class PrintView {
+    public static void printMyView(int i, ToDoItem item)
+    {
+        System.out.print(i + ") [");
+        if (item.isComplete())
+        {
+            System.out.print("*");
+        }
+        else
+        {
+            System.out.print(" ");
+        }
+        System.out.println("] " + item.getToDoText());
+    }
+}
